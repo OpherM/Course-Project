c9a6d3047278132c3626a78703dd727e151a3b40
OpherM <47371176+OpherM@users.noreply.github.com>
2023-06-12 16:56:21 +0100

    rolling

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -13,7 +13,6 @@ const Register = () => {
                     </h1>
                     <p className='text-lg mt-4 text-gray-600'>Start learning by registering for free</p>
                     <button className='my-4 bg-[#20B486] px-8 py-3.5 rounded-md text-white font-bold max-[780px]:w-full'>Sign Up for Free</button>
-
                 </div>
             </div>
         </div>
